396b2b2cc24b08f25be5ad1a083231164594cb04
jhgarcesz <jhgarcesz@gmail.com>
2018-10-26 17:28:37 -0500

    add product list component

---
+++ src/components/ProductList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import ProductItem  from "./ProductItem";
+
+import {
+  List
+} from 'react-md';
+
+const ProductList = ({ products }) => (
+    <List className="md-list-unstyled md-grid avatars__examples">
+        {products.map(product => (
+            <ProductItem detail= { product }  key= { product.id } />
+        ))}
+    </List>
+);
+
+ProductList.propTypes = {
+    products: PropTypes.arrayOf(PropTypes.object)
+};
+
+ProductList.defaultProps = {
+    products: []
+};
+
+export default ProductList;
